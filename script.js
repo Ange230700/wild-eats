412e51b255bf5e934ed1d56745d2fa058afdf315
CarolineG85 <caroline.gallix85@gmail.com>
2023-09-24 15:39:04 +0200

    contactForm html and css modified, a comment on js created

---
+++ script.js
@@ -0,0 +1,5 @@
+/*document.getElementById("contactForm").addEventListener('submit', function(event) {
+    event.preventDefault();
+    const message = "Email envoyé! Nous vous recontacterons bientôt";
+    alert(message);
+});*/
