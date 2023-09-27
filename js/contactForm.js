436893d2c9017d7926d2181267d9029f84888ae0
CarolineG85 <caroline.gallix85@gmail.com>
2023-09-27 12:37:14 +0200

    trying js addlistener for the button

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,10 +1,11 @@
 
     //const message = "Email envoyé! Nous vous recontacterons bientôt";
-    
+ //créer class dans css, voir si animation, appendchild   
 
 const button = document.querySelector("#submit")
 
-/*button.addEventListener("click", () =>
+button.addEventListener("click", () => {
+    const message = document.createElement("div")
+    message.classList("email-send")
 
-
-)*/
+})
