9bcac469a326b69a3f4e5a351a424fcf64af5dfa
CarolineG85 <caroline.gallix85@gmail.com>
2023-09-27 11:59:29 +0200

    contactForm.js created, header added, other files modified

---
+++ js/contactForm.js
@@ -0,0 +1,10 @@
+
+    //const message = "Email envoyé! Nous vous recontacterons bientôt";
+    
+
+const button = document.querySelector("#submit")
+
+/*button.addEventListener("click", () =>
+
+
+)*/
--- script.js
+++
@@ -1,5 +0,0 @@
-/*document.getElementById("contactForm").addEventListener('submit', function(event) {
-    event.preventDefault();
-    const message = "Email envoyé! Nous vous recontacterons bientôt";
-    alert(message);
-});*/
