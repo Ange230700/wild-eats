2cde24810ce67df2333b972354f1c2cc92ebdd17
CarolineG85 <caroline.gallix85@gmail.com>
2023-10-03 12:06:43 +0200

    hover added on inputs

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,12 +1,9 @@
-
-    //const message = "Email envoyé! Nous vous recontacterons bientôt";
- //créer class dans css, voir si animation, appendchild   
-
+  
 const form = document.querySelector("#contactForm")
 const textarea = document.querySelector("#textarea")
 const header = document.querySelector("header")
 const message = document.querySelector(".message")
-const input = document.querySelector("#submit")
+const submit = document.querySelector("#submit")
 
 
 form.addEventListener("submit", (event) =>  {
